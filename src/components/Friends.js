563197d89040ccdaf05fa67fcc4e3cd8f184317e
sm-muhaimen <amuhaimenpro829@gmail.com>
2023-01-31 14:21:50 +0600

    friend request 2

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  set,
+  remove,
+  push,
+} from "firebase/database";
+import { useSelector } from "react-redux";
 
 const Friends = () => {
+  const db = getDatabase();
+  let [friends, setFriends] = useState([]);
+  let data = useSelector((state) => state);
+  console.log(data.userdata.userInfo.uid);
+
+  useEffect(() => {
+    const starCountRef = ref(db, "friends");
+    onValue(starCountRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        if (
+          data.userdata.userInfo.uid == item.val().receiverid ||
+          data.userdata.userInfo.uid == item.val().senderid
+        ) {
+          arr.push({ ...item.val(), id: item.key });
+        }
+      });
+      setFriends(arr);
+    });
+  }, []);
   return (
     <div className="grouplistholder">
       <div className="titleholder">
@@ -8,18 +37,24 @@ const Friends = () => {
       </div>
 
       <div className="boxholder">
-        <div className="box">
-          <div className="boximgholder">
-            <img src="assets/profile.png" />
+        {friends.map((item) => (
+          <div className="box">
+            <div className="boximgholder">
+              <img src="assets/profile.png" />
+            </div>
+            <div className="title">
+              {data.userdata.userInfo.uid == item.senderid ? (
+                <h3>{item.receivername}</h3>
+              ) : (
+                <h3>{item.sendername}</h3>
+              )}
+              <p>{item.Date}</p>
+            </div>
+            <div>
+              <button className="boxbtn">Block</button>
+            </div>
           </div>
-          <div className="title">
-            <h3>Friends Reunion</h3>
-            <p>Hi Guys, Wassup!</p>
-          </div>
-          <div>
-            <button className="boxbtn">Block</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
